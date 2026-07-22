20ca3c5ae4996e4508e549279b1aea3e9ac81117
Arya Choudhari <choudhariarya73@gmail.com>
2026-07-22 21:30:04 +0530

    Remove unused numpy and category imports
    Removed unused imports to clean up the code.

--- Heart-Disease-Prediction/training.py
+++ Heart-Disease-Prediction/training.py
@@ -1,7 +1,5 @@
 import pandas as pd
-import numpy as np
 import joblib
-from pandas.core.indexes import category
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
